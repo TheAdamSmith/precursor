2a65c0b6f0b0c233d7fffd5ca24ae846f0bfe4be
mileszcoker <mileszcoker@gmail.com>
2024-02-09 19:37:49 -0700

    Added basic character
    Added model
    Added movement

---
+++ characters/player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+signal hit
+
+@export var speed = 400 # Player speed
+var screen_size # size of game window
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	#to do: uncomment
+	#hide()
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	#movement and animation
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite2D.rotation = PI/2
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		$AnimatedSprite2D.rotation = 3*PI/2
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		$AnimatedSprite2D.rotation = PI
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		$AnimatedSprite2D.rotation = 0
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
