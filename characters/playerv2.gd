6362ffd53cae07ede7cf620f38e999e982be8080
mileszcoker <mileszcoker@gmail.com>
2024-02-11 11:01:18 -0700

    Update per style guide

--- characters/playerv2.gd
+++ characters/playerv2.gd
@@ -5,14 +5,18 @@ signal hit
 @onready var animations = $AnimationPlayer
 var screen_size # size of game window
 
+
 func _ready():
 	screen_size = get_viewport_rect().size
 	#to do: uncomment
 	#hide()
 
+
 func handleInput():
 	var moveDirection = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = moveDirection * speed
+
+
 func updateAnimation():
 	if velocity.length() ==0:
 		animations.stop()
@@ -23,18 +27,22 @@ func updateAnimation():
 		elif velocity.y < 0: direction = "Up"
 		
 		animations.play("walk" + direction)
-	
+
+
 func _process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
-	
+
+
 func _on_body_entered(body):
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
 
+
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+
