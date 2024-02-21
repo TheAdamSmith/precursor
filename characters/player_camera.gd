09fca146ce12dfa0ee04ae47f6e0c11f8cea435c
Will Chaney <wills.apps@yahoo.com>
2024-02-20 21:22:39 -0700

    add camera zoom and pan controls

---
+++ characters/player_camera.gd
@@ -0,0 +1,89 @@
+class_name PlayerCamera
+extends Camera2D
+
+# zoom vars
+@export var zoom_max = Vector2(.7, .7)
+@export var zoom_min = Vector2(3, 3)
+@export var zoom_lerp_speed = 4.0
+@export var zoom_speed = .5
+@export var zoom_total_time = 1
+
+var zoom_out = false
+var zoom_in = false
+
+var time_lerping := 0.0
+var target_zoom: Vector2
+
+# pan vars
+@export var pan_lerp_speed: float = 4
+@export var pan_grab_frames: int = 4
+@export var pan_velocity_dampening_factor: float = 5
+@export var pan_velocity_div_factor: float = 150
+
+var pan_grab_pos: Vector2
+var panning: bool
+var pan_velocity: Vector2
+var pos_holder: Array = []
+
+func _unhandled_input(event: InputEvent):
+	if Input.is_action_just_released("zoom_out") and zoom > zoom_max:
+		zoom_out = true
+		zoom_in = false
+		target_zoom = zoom - Vector2(zoom_speed, zoom_speed)
+		time_lerping = 0
+	if Input.is_action_just_released("zoom_in") and zoom < zoom_min:
+		zoom_in = true
+		zoom_out = false
+		target_zoom = zoom + Vector2(zoom_speed, zoom_speed)
+		time_lerping = 0
+
+	if Input.is_action_just_pressed("camera_pan_grab"):
+		panning = true
+		pan_grab_pos = event.position
+		pan_velocity = Vector2.ZERO
+		pos_holder.clear()
+	elif Input.is_action_just_released("camera_pan_grab"):
+		if panning and pan_velocity != Vector2.ZERO:
+			pan_velocity = pan_velocity.normalized() * (pan_velocity.length() / pan_velocity_div_factor)
+			
+		panning = false
+
+	if Input.is_action_just_pressed("center_player"):
+		offset = Vector2.ZERO
+		pan_velocity = Vector2.ZERO
+
+
+func _physics_process(delta: float):
+	if panning:
+		var curr_mouse_pos = get_viewport().get_mouse_position()
+
+		# keep track of mouse pos for $pan_grab_frames to generate a velocity
+		# vector for the camera once "camera_pan_grab" is released
+		# done in _physics_process so that FPS doesn't impact the pan velocity
+		pos_holder.push_back(curr_mouse_pos)
+		if len(pos_holder) == pan_grab_frames + 1:
+			pos_holder.pop_front()
+		pan_velocity = pos_holder[0] - pos_holder[len(pos_holder) - 1]
+
+
+func _process(delta):
+	if zoom_out == true and zoom >= zoom_max:
+		time_lerping += delta
+		zoom = zoom.slerp(target_zoom, zoom_lerp_speed * delta)
+		if time_lerping >= zoom_total_time:
+			zoom_out = false
+
+	if zoom_in == true and zoom <= zoom_min:
+		time_lerping += delta
+		zoom = zoom.slerp(target_zoom, zoom_lerp_speed * delta)
+		if time_lerping >= zoom_total_time:
+			zoom_out = false
+
+	if panning:
+		var curr_mouse_pos = get_viewport().get_mouse_position()
+		offset += pan_grab_pos - curr_mouse_pos
+		pan_grab_pos = curr_mouse_pos
+
+	if not panning:
+		offset += pan_velocity
+		pan_velocity = pan_velocity.slerp(Vector2.ZERO, pan_velocity_dampening_factor * delta)
