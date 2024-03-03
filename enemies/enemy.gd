b0345838d37da6a3f921b6d392291fce74559f95
ninja-rage <ruslanoliynyk2516@gmail.com>
2024-03-03 15:10:09 -0700

    Update enemy.gd

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var health = 3
+var speed = 100.0
 
 # TODO modify once mobs are generated via script, 
 # should not be hard coding to find the file
@@ -8,10 +9,19 @@ var health = 3
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 100.0
 	
-	#check to see how many objects colliding with the mob
+	var direction = global_position.direction_to(player.global_position)
+	
+	# flip the animated sprite body in the direction of travel
+	if direction.x > 0:
+		_animated_sprite.set_scale(Vector2(-1, 1))
+	else :
+		_animated_sprite.set_scale(Vector2(1, 1))
+	
+	velocity = direction * speed
+	
+	# check to see how many objects colliding with the mob
+	# play attack animation on collision
 	var overlaps = %AttackBox.get_overlapping_bodies()
 	if (overlaps.has(player)):
 		_animated_sprite.play("attack")
