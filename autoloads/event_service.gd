f34e3c615b8422f7b86c719c5a0d045ccd086ae6
Will Chaney <wills.apps@yahoo.com>
2024-03-21 18:06:02 -0600

    cleanup

--- autoloads/event_service.gd
+++ autoloads/event_service.gd
@@ -13,10 +13,6 @@ signal entity_death(damaging_entity, dying_entity)
 enum GAME_STATE {MENU, IN_PROGRESS}
 var game_state
 
-var level_bgm_mappings: Dictionary = {
-	
-}
-
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -40,6 +36,8 @@ func _input(event):
 
 func _change_scene(scene_path):
 	if scene_path.begins_with("res://ui"):
+		# when returning to main menu, stop BGM for now
+		# in future, this would be replaced to play the main menu music
 		if scene_path == "res://ui/main_menu.tscn":
 			SoundManager.stop_bgm()
 		if game_state == GAME_STATE.IN_PROGRESS:
@@ -52,6 +50,8 @@ func _change_scene(scene_path):
 
 func _start_game():
 	game_state = GAME_STATE.IN_PROGRESS
+	# will need to be adjusted when there are more than one scene associated
+	# with "starting the game"
 	SoundManager.play_bgm(load("res://assets/audio/bgm/planet_level_1.wav"))
 	var next_scene = load("res://levels/planet_level/planet_level.tscn")
 	get_tree().change_scene_to_packed(next_scene)
