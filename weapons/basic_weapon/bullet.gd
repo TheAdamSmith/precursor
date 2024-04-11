bca317f7a49cd59ad237d892e720e4e9b8093ecc
Adam Smith <12asmith3@gmail.com>
2024-04-11 07:17:39 -0700

    more cleanup

--- weapons/basic_weapon/bullet.gd
+++ weapons/basic_weapon/bullet.gd
@@ -16,11 +16,9 @@ func _on_body_entered(body):
 	linear_velocity.y = 0
 	$Vfx.show()
 	$Vfx.play()
-	
 	EventService.entity_damaged.emit(self, body, damage)
 	await get_tree().create_timer(0.05).timeout 
 	$SmallBullet.hide()
-	queue_free()
 
 
 func _on_vfx_animation_looped():
