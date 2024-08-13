1a9b3b28556b0557d030e2a34713b3a76be3edef
mitchellkazin15 <mitchell.kazin15@gmail.com>
2024-08-13 17:50:29 -0600

    starting to flesh out displaceable characters more

--- characters/displaceable_character_body.gd
+++ characters/displaceable_character_body.gd
@@ -1,5 +1,25 @@
 class_name DisplaceableCharacterBody2D
 extends CharacterBody2D
 
-
 @export var mass : float
+@export var movement_acceleration : float
+
+
+var move_direction : Vector2 = Vector2.ZERO :
+	set(dir):
+		move_direction = dir.normalized()
+	get:
+		return move_direction
+
+
+func apply_impulse(impulse_vec : Vector2):
+	velocity += impulse_vec / mass
+
+
+func _physics_process(delta):
+	velocity += move_direction * movement_acceleration * delta
+	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		print("Collided with: ", collision.get_collider())
+
