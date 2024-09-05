c1e3c90964f1c1b9088dbafe41d426750eea8df6
mjkazin <mjkazin@iastate.edu>
2024-09-04 19:03:24 -0600

    moved cooldown icon to be part of hud, and removed knockback from dash upgrades

--- characters/player/ability_icon.gd
+++ characters/player/ability_icon.gd
@@ -3,17 +3,17 @@ extends Sprite2D
 
 @onready var cooldown_progress = $TextureProgressBar
 
-var _cooldown_timer : SceneTreeTimer
+@export var ability_component : DashComponent
+
+var _prev_timer : SceneTreeTimer
 var _total_time_sec : float
 
 
-func set_timer(cooldown_timer):
-	_cooldown_timer = cooldown_timer
-	_total_time_sec = cooldown_timer.time_left
-
-
 func _process(delta):
-	if _cooldown_timer:
-		cooldown_progress.value = cooldown_progress.max_value * (1 - _cooldown_timer.time_left / _total_time_sec)
+	if ability_component.cooldown_timer and ability_component.cooldown_timer != _prev_timer:
+		_prev_timer = ability_component.cooldown_timer
+		_total_time_sec = ability_component.cooldown_timer.time_left
+	if _prev_timer:
+		cooldown_progress.value = cooldown_progress.max_value * (1 - _prev_timer.time_left / _total_time_sec)
 	else:
 		cooldown_progress.value = cooldown_progress.max_value
