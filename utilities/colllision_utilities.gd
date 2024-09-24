2bb762eb48c9f4bc55a4d5792fec6c3420cddbc2
mitchellkazin15 <mitchell.kazin15@gmail.com>
2024-09-24 17:49:46 -0600

    fixed bug where intangibility still caused collisions with enemies

--- utilities/colllision_utilities.gd
+++ utilities/colllision_utilities.gd
@@ -1,8 +1,19 @@
 class_name CollisionUtilities
 
+const LEVEL_FLAG  = 0b00000000000000000000000000000001
+const PLAYER_FLAG = 0b00000000000000000000000000000010
+const ENEMY_FLAG  = 0b00000000000000000000000000000100
+const DAMAGE_FLAG = 0b00000000000000000000000000001000
+const AOE_FLAG    = 0b00000000000000000000000000010000
 
-const LEVEL_MASK  = 0b1
-const PLAYER_MASK = 0b10
-const ENEMY_MASK  = 0b100
-const DAMAGE_MASK = 0b1000
-const AOE_MASK    = 0b10000
+
+static func set_flag(mask, flag):
+	return mask | flag
+
+
+static func clear_flag(mask, flag):
+	return mask & ~flag
+
+
+static func toggle_flag(mask, flag):
+	return mask ^ flag
