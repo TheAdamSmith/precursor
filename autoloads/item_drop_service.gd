2417e997972f9a3c4ecf439df493361aa3893a23
mitchellkazin15 <mitchell.kazin15@gmail.com>
2024-10-09 20:54:52 -0600

    Added spawn chance and item lifetime timer

--- autoloads/item_drop_service.gd
+++ autoloads/item_drop_service.gd
@@ -8,6 +8,8 @@ func _ready():
 
 
 func _on_item_drop_event(item_level, item_tier, item_position, drop_chance):
+	if randf_range(0.0, 1.0) > drop_chance:
+		return
 	var item = load("res://items/healing_item.tscn").instantiate()
 	item.global_position = item_position
 	add_child(item)
--- items/item.gd
+++ items/item.gd
@@ -4,9 +4,15 @@ extends Node2D
 @export var pickup_on_contact = true
 @export var use_on_pickup = true
 @export var free_on_pickup = true
+@export var lifetime_on_ground_sec = INF
+
+var lifetime_timer : SceneTreeTimer
 
 
 func _ready():
+	if lifetime_on_ground_sec > 0.0 and lifetime_on_ground_sec != INF:
+		lifetime_timer = get_tree().create_timer(lifetime_on_ground_sec)
+		lifetime_timer.timeout.connect(_on_lifetime_timeout)
 	if pickup_on_contact:
 		$Area2D.body_entered.connect(_on_pickup)
 
@@ -17,6 +23,10 @@ func _on_pickup(body):
 		use(body)
 	if free_on_pickup:
 		queue_free()
+
+
+func _on_lifetime_timeout():
+	queue_free()
 
 
 func pickup(picking_up_entity):
